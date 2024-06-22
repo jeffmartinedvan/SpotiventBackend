b8e3d403d9ba7db937296e5d5b4e5dc014d03743
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-06-22 15:05:32 +0700

    first commit

---
+++ src/main/java/com/Spotivent/SpotiventBackend/SpotiventBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Spotivent.SpotiventBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotiventBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotiventBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spotivent/SpotiventBackend/SpotiventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spotivent.SpotiventBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotiventBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
