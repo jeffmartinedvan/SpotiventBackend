60d47d5dcabb613d8cb4f33f771b985d5d489173
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-07-03 13:48:14 +0700

    feat: initialize authentication and authorization feature

--- src/main/java/com/Spotivent/SpotiventBackend/SpotiventBackendApplication.java
+++ src/main/java/com/Spotivent/SpotiventBackend/SpotiventBackendApplication.java
@@ -1,11 +1,13 @@
 package com.Spotivent.SpotiventBackend;
 
+import com.Spotivent.SpotiventBackend.config.RsaKeyConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyConfigProperties.class)
 public class SpotiventBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpotiventBackendApplication.class, args);
 	}
--- src/main/java/com/Spotivent/SpotiventBackend/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/Spotivent/SpotiventBackend/users/service/impl/UserServiceImpl.java
@@ -4,21 +4,25 @@ import com.Spotivent.SpotiventBackend.users.dto.RegisterRequestDTO;
 import com.Spotivent.SpotiventBackend.users.entity.Users;
 import com.Spotivent.SpotiventBackend.users.repository.UserRepository;
 import com.Spotivent.SpotiventBackend.users.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public Users register(RegisterRequestDTO registerRequestDTO) {
         Users newUser = new Users();
         newUser.setEmail(registerRequestDTO.getEmail());
-        newUser.setPassword(registerRequestDTO.getPassword());
+        var password = passwordEncoder.encode(registerRequestDTO.getPassword());
+        newUser.setPassword(password);
         newUser.setUsername(registerRequestDTO.getUsername());
         newUser.setRole(registerRequestDTO.getRole());
         return userRepository.save(newUser);
