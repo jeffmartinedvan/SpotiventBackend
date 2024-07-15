4b9b453d9896abc6d9ae864cfbc976c7ef339b18
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-07-15 23:15:35 +0700

    fix: restcontroller annotation

--- src/main/java/com/Spotivent/SpotiventBackend/events/controller/EventController.java
+++ src/main/java/com/Spotivent/SpotiventBackend/events/controller/EventController.java
@@ -10,13 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("api/v1/event")
 @Validated
 public class EventController {
