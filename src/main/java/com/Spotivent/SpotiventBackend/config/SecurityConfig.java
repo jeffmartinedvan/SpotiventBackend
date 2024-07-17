a73d2f7379a3ce705243456818a72f78d746a361
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-07-17 14:37:24 +0700

    feat: add endpoint request matcher

--- src/main/java/com/Spotivent/SpotiventBackend/config/SecurityConfig.java
+++ src/main/java/com/Spotivent/SpotiventBackend/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
                     auth.requestMatchers("/api/v1/user/register").permitAll();
                     auth.requestMatchers("/api/v1/tickets/**").hasRole("ORGANIZER");
                     auth.requestMatchers("/api/v1/event/create").hasRole("ORGANIZER");
+                    auth.requestMatchers("/api/v1/coupon/create").hasRole("ORGANIZER");
                     auth.anyRequest().authenticated();
                 })
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
